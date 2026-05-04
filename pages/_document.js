8c61490a82cf8dfe265dbd6f8c1c4f4d99ec0220
gilmartamoios-ops <gilmartamoios@gmail.com>
2026-05-04 00:26:25 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html translate="no">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

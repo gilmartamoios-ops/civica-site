7208ae2b8fc6200b33156702c0b3f621467a532e
gilmartamoios-ops <gilmartamoios@gmail.com>
2026-05-03 22:53:10 -0300

    Update [uf].js

--- pages/estado/[uf].js
+++ pages/estado/[uf].js
@@ -1,1 +1,44 @@
+import { useRouter } from "next/router";
 
+import ba from "../../data/editoriais/ba.json";
+import ce from "../../data/editoriais/ce.json";
+import mg from "../../data/editoriais/mg.json";
+import pe from "../../data/editoriais/pe.json";
+import rj from "../../data/editoriais/rj.json";
+import sp from "../../data/editoriais/sp.json";
+
+const dados = { ba, ce, mg, pe, rj, sp };
+
+export default function Estado() {
+  const router = useRouter();
+  const { uf } = router.query;
+
+  const data = dados[uf];
+
+  if (!data) return null;
+
+  return (
+    <div className="container">
+
+      <button onClick={() => router.push("/")}>
+        ← Voltar
+      </button>
+
+      <div 
+        className="capa"
+        style={{ backgroundImage: `url(${data.imagem_capa})` }}
+      >
+        <h2>{data.titulo}</h2>
+        <span>{data.mes}</span>
+      </div>
+
+      <div className="autor">
+        <img src={data.imagem_lideranca} />
+        <strong>{data.autor}</strong>
+      </div>
+
+      <p className="texto">{data.conteudo}</p>
+
+    </div>
+  );
+}

56344a4ff04894d7037de43ae39d00f47ccf23b3
gilmartamoios-ops <gilmartamoios@gmail.com>
2026-05-07 19:25:41 -0300

    Update MenuEstados.js

--- components/MenuEstados.js
+++ components/MenuEstados.js
@@ -37,7 +37,10 @@ export default function MenuEstados() {
       <button onClick={() => router.push("/manifesto")}>
         MANIFESTO
       </button>
-<button onClick={() => router.push("/rede")}>
+<button
+  className="botao-rede"
+  onClick={() => router.push("/rede")}
+>
   PARTICIPAR DA REDE
 </button>
           
